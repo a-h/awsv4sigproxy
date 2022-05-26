5910c75cda069f0212a75ae8d689d0fbe97858d3
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-05-26 16:40:33 +0100

    feat: add support for Lambda function URLs

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ import (
 
 var remoteFlag = flag.String("remote", "", "The base URL of the remote API to call, e.g. https://ej9a7qm7ye.execute-api.eu-west-1.amazonaws.com")
 var localFlag = flag.String("local", "localhost:6666", "The local address to listen on.")
+var serviceFlag = flag.String("service", "execute-api", "The service (execute-api for API Gateway, lambda for Lambda function URLs).")
 
 func main() {
 	flag.Parse()
@@ -29,13 +30,13 @@ func main() {
 		os.Exit(1)
 		return
 	}
-	if err := run(*localFlag, *remoteFlag); err != nil {
+	if err := run(*localFlag, *remoteFlag, *serviceFlag); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run(local, remote string) error {
-	p, messages, errors, err := NewProxy(remote)
+func run(local, remote string, service string) error {
+	p, messages, errors, err := NewProxy(remote, service)
 	if err != nil {
 		return err
 	}
@@ -53,12 +54,12 @@ func run(local, remote string) error {
 	return http.ListenAndServe(local, p)
 }
 
-func NewProxy(baseURL string) (p http.Handler, messages chan string, errors chan error, err error) {
+func NewProxy(baseURL string, service string) (p http.Handler, messages chan string, errors chan error, err error) {
 	u, err := url.Parse(baseURL)
 	if err != nil {
 		return nil, nil, nil, err
 	}
-	signer, err := NewSigner(u)
+	signer, err := NewSigner(u, service)
 	if err != nil {
 		return nil, nil, nil, err
 	}
@@ -74,11 +75,12 @@ type Signer struct {
 	cfg      aws.Config
 	region   string
 	now      func() time.Time
+	service  string
 	Messages chan string
 	Errors   chan error
 }
 
-func NewSigner(u *url.URL) (c Signer, err error) {
+func NewSigner(u *url.URL, service string) (c Signer, err error) {
 	c.cfg, err = config.LoadDefaultConfig(context.Background())
 	if err != nil {
 		return
@@ -87,6 +89,7 @@ func NewSigner(u *url.URL) (c Signer, err error) {
 	c.region = c.cfg.Region
 	c.signer = v4.NewSigner()
 	c.now = time.Now
+	c.service = service
 	c.Errors = make(chan error)
 	c.Messages = make(chan string)
 	return
@@ -128,7 +131,7 @@ func (c Signer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		c.Errors <- fmt.Errorf("v4sigproxy: failed to retrieve creds: %w", err)
 		return
 	}
-	err = c.signer.SignHTTP(context.Background(), creds, req, hash, "execute-api", c.region, c.now())
+	err = c.signer.SignHTTP(context.Background(), creds, req, hash, c.service, c.region, c.now())
 	if err != nil {
 		c.Errors <- fmt.Errorf("v4sigproxy: failed to sign request: %w", err)
 		return
